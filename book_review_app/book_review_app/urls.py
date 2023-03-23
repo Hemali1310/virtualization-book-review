aedb648efa0c1892cca61f313b02d5c7ba056848
Hemali <hemali@Hemalis-MacBook-Air.local>
2023-03-23 12:45:28 -0400

    file structure for web page

--- book_review_app/book_review_app/urls.py
+++ book_review_app/book_review_app/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from book_review_app.views import home
-
+from book_review_app.views import home_page, login_page, signup_page, browse_page, book_review_page, user_profile, author_profile, subscription, payment_page
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('', home)
+    path('', home_page),
+    path('login/', login_page),
+    path('signup/', signup_page),
+    path('browse/', browse_page),
+    path('book_review/', book_review_page),
+    path('user_profile/', user_profile),
+    path('author_profile/', author_profile),
+    path('subscription/', subscription),
+    path('payment/', payment_page)
 ]
--- book_review_app/book_review_app/views.py
+++ book_review_app/book_review_app/views.py
@@ -2,13 +2,39 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def home(request):
-    return render(request, "home.html")
+def home_page(request):
+    data = {}
+    data["title"] = "Test Page"
+    return render(request, "home.html", data)
 
 
-def login(request):
-    return render(request, "login.html")
+def login_page(request):
+    return render(request, "login_page.html")
 
 
-def signup(request):
-    return render(request, "signup.html")
+def signup_page(request):
+    return render(request, "signup_page.html")
+
+
+def browse_page(request):
+    return render(request, "browse.html")
+
+
+def book_review_page(request):
+    return render(request, "book_review_page.html")
+
+
+def user_profile(request):
+    return render(request, "user_profile.html")
+
+
+def author_profile(request):
+    return render(request, "author_profile.html")
+
+
+def subscription(request):
+    return render(request, "subscription.html")
+
+
+def payment_page(request):
+    return render(request, "payment_page.html")
